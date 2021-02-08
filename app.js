7959a248d1c8a67055b52844147a54bee2a02ec4
prosourav <sourav19961010@gmail.com>
2021-02-08 17:49:43 +0530

    all features added

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ const button = document.getElementById("btn").addEventListener("click",()=>{
        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${dishName}`)
        .then(res => res.json())
        .then(data=>{
-       displayData(data.meals);
-    //    console.log(data.meals);
-      
-       });
+        displayData(data.meals)})
+       .catch(error => displayError("something went wrong! please try again later"));
+       
  const displayData = (items) => {
    const dishList = document.getElementById("dish-list");
     items.forEach((dish) => {
@@ -35,10 +34,11 @@ const button = document.getElementById("btn").addEventListener("click",()=>{
      console.log(name);
   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`)
   .then(res => res.json())
-  .then(data => displayDetail(data.meals[0]));
-
+  .then(data => displayDetail(data.meals[0]))
+  .catch(error => displayError("something went wrong! please try again later"));
+ 
   const displayDetail=(item)=>{
-      console.log(item);
+    //   console.log(item);
       const sec = document.getElementById("dish-detail");
     // console.log(item.strIngredient1);
     // console.log(item.strIngredient3);
@@ -49,6 +49,9 @@ const button = document.getElementById("btn").addEventListener("click",()=>{
  <h4>${item.strIngredient1}<h4>
  <h4>${item.strIngredient2}<h4>
  <h4>${item.strIngredient3}<h4>
+ <h4>${item.strIngredient4}<h4>
+ <h4>${item.strIngredient5}<h4>
+ <h4>${item.strIngredient6}<h4>
   `
   const div = document.createElement("div");
   div.className="ingredients";
@@ -56,3 +59,9 @@ const button = document.getElementById("btn").addEventListener("click",()=>{
   sec.appendChild(div);
   }
  }
+const displayError=(error)=>{
+    const section = document.getElementById("dish-detail");
+    const h1 = document.createElement("h1");
+    h1.innerText = "Sorry error occurd! pleae try again";
+    section.appendChild(h1);
+}
